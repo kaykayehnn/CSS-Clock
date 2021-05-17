e9093d89bc0c1f1dd1e697eeb1acacde9d1ec1ca
Krasimir Nedelchev <kaykayehnn@gmail.com>
2021-05-17 13:51:45 +0300

    Initial commit

---
+++ clock.js
@@ -0,0 +1,17 @@
+let date = new Date();
+
+let seconds = date.getSeconds();
+let minutes = date.getMinutes();
+let hours = date.getHours();
+
+let hoursHand = document.querySelector(".hours-hand-container");
+let minutesHand = document.querySelector(".minutes-hand-container");
+let secondsHand = document.querySelector(".seconds-hand-container");
+
+let secondsDegrees = (seconds / 60) * 360;
+let minutesDegrees = ((minutes * 60 + seconds) / 3600) * 360;
+let hoursDegrees = (((hours % 12) * 3600 + minutes * 60 + seconds) / 86400) * 360;
+
+secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
